112f9e111048309ad760027501d5f272a28ab493
Ritanshu Tiwari <ritanshutiwari100@gmail.com>
2026-05-31 20:35:34 +0530

    Time: 30 ms (5.66%) | Memory: 46.8 MB (5.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,30 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character,Integer>map1= new HashMap<>();
+        HashMap<Character,Integer>map2= new HashMap<>();
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(map1.containsKey(ch)){
-                int freq=map1.get(ch);
-                map1.put(ch,freq+1);
+            char ch1= s.charAt(i);
+            char ch2= t.charAt(i);
+            if(map1.containsKey(ch1)){
+                map1.put(ch1,map1.getOrDefault(ch1,0)+1);
+            }else{
+                map1.put(ch1,1);
             }
-            else map1.put(ch,1);
-        } 
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(map2.containsKey(ch)){
-                int freq=map2.get(ch);
-                map2.put(ch,freq+1);
+            if(map2.containsKey(ch2)){
+                map2.put(ch2,map2.getOrDefault(ch2,0)+1);
+            }else{
+                map2.put(ch2,1);
             }
-            else map2.put(ch,1);
+        }
+            for(char ch: map1.keySet()){
+                if(!map2.containsKey(ch)) return false;
+                if(!map1.get(ch).equals(map2.get(ch)))
+                return false;
+        }
+        return true;
     }
-    for(char key:map1.keySet()){
-        int val1 = map1.get(key);
-        if(!map2.containsKey(key)) return false;
-        int val2 = map2.get(key);
-        if(val1!=val2) return false;
-    }
-    return true;
-}
 }
