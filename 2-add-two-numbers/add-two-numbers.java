ace3ba0d5b9c237a13f3525a66d5dcb4b2d7606b
Ritanshu Tiwari <ritanshutiwari100@gmail.com>
2026-06-09 21:05:24 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (75.07%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,40 +1,22 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int[] arr1=new int[100];
-        int[] arr2=new int[100];
-        int[] arr3=new int[101];
-        ListNode temp1=l1;
-        ListNode temp2=l2;
-        int n1=0, n2=0;
-        // Fill arr1
-        while (temp1!=null) {
-            arr1[n1++]=temp1.val;
-            temp1=temp1.next;
-        }
-        // Fill arr2
-        while(temp2!=null) {
-            arr2[n2++]=temp2.val;
-            temp2=temp2.next;
-        }
-        int len=Math.max(n1, n2);
-        int carry=0;
-        // Add the digits
-        for (int i=0;i<len;i++) {
-            int sum=arr1[i]+arr2[i]+carry;
-            arr3[i]=sum%10;
+        ListNode l3 = new ListNode(0);
+        ListNode curr=l3;
+        int carry =0;
+        while(l1!=null || l2!=null || carry!=0){
+            int sum=carry;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
             carry=sum/10;
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
         }
-        if (carry>0) {
-            arr3[len]=carry;
-            len++;
-        }
-        // Convert arr3 to linked list
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        for (int i = 0; i < len; i++) {
-            curr.next = new ListNode(arr3[i]);
-            curr = curr.next;
-        }
-        return dummy.next;
+        return l3.next;
     }
 }
