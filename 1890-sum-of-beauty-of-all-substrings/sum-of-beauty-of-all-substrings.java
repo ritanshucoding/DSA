a08eeefef626f96b0054029d1cf0ca582ac02615
Ritanshu Tiwari <ritanshutiwari100@gmail.com>
2026-07-24 20:25:17 +0530

    Time: 522 ms (14.83%) | Memory: 47.1 MB (9.69%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int beautySum(String s) {
+        int ans =0;
+        for(int i=0;i<s.length();i++){
+            HashMap<Character , Integer> map = new HashMap<>();
+            for(int j=i;j<s.length();j++){
+                char ch = s.charAt(j);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+            int max=0;
+            int min = Integer.MAX_VALUE;
+            for(int freq:map.values()){
+                max = Math.max(max,freq);
+                min = Math.min(min,freq);
+            }
+            ans+=(max-min);
+            }}
+            return ans;
+        }
+    }
