896cb82d1b62e94a402e69a1faa18f9eef2982a4
Ritanshu Tiwari <ritanshutiwari100@gmail.com>
2026-08-11 16:37:40 +0530

    Time: 4 ms (97.66%) | Memory: 45.4 MB (46.34%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,18 +1,31 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st = new Stack<>();
-        for(String t: tokens){
-            if(t.equals("+")|| t.equals("-")|| t.equals("*")||t.equals("/")){
-                int b =st.pop();
-                int a = st.pop();
-                if(t.equals("+")) st.push(a+b);
-                else if(t.equals("-")) st.push(a-b);
-                else if(t.equals("*")) st.push(a*b);
-                else st.push(a/b);
-            }else{
-                st.push(Integer.parseInt(t));
+        int[] num= new int[tokens.length];
+        char[] op= new char[tokens.length];
+        int ntop=-1;
+        int otop=-1;
+        for(String t:tokens){
+            if(!t.equals("+") && !t.equals("-") && !t.equals("*") && !t.equals("/")){
+                num[++ntop]=Integer.parseInt(t);
+            }
+            else{
+                op[++otop]=t.charAt(0);
+                char operator=op[otop--];
+                int b = num[ntop--];
+                int a = num[ntop--];
+                int result=0;
+                if(operator=='+'){
+                    result=a+b;
+                }else if(operator=='-'){
+                    result=a-b;
+                }else if(operator=='*'){
+                    result=a*b;
+                }else if(operator=='/'){
+                    result=a/b;
+                }
+                num[++ntop]=result;
             }
         }
-        return st.peek();
+        return num[ntop];
     }
 }
