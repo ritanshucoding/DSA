cf7fed5be8ff769c704b659cbd3a898926e98cb9
Ritanshu Tiwari <ritanshutiwari100@gmail.com>
2026-08-19 00:01:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (89.74%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -5,14 +5,12 @@ class Solution {
         for(char ch:s.toCharArray()){
             if(ch=='('){
                 open++;
-            }else{
-                if(open>0){
+            }else if(ch==')' && open>0){
                     open--;
                 }else{
                     ans++;
                 }
             }
-        }
         return ans+open;
     }
 }
